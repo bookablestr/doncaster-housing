ce2894108cc3fe598b00e13152b848ee1e7529b8
Nnenna Udefi <miriamudefi@gmail.com>
2025-03-24 20:36:28 +0100

    Update page.tsx

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 export default function GalleryPage() {
   return (
-    <div>
+    <div className="text-black">
       <Gallery />
       <ThirdParty />
       <Footer />
