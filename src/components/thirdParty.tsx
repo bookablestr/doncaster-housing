d70cb94f6e3517ba1aa1a9a4210b6772e5bb5939
Nnenna Udefi <miriamudefi@gmail.com>
2025-03-24 20:27:16 +0100

    Update thirdParty.tsx

--- src/components/thirdParty.tsx
+++ src/components/thirdParty.tsx
@@ -3,7 +3,7 @@ import { DarkButton } from "./ui/darkButton";
 
 const ThirdParty = () => {
   return (
-    <div className="lg:px-12 px-6 lg:py-16 py-10">
+    <div className="lg:px-12 px-6 lg:py-16 py-10" text-black>
       <div className="custom-third-gradient py-20  rounded-2xl  gap-5 flex flex-col justify-center items-center">
         <p className="font-quicksand lg:text-4xl text-2xl font-bold">
           Want a <span className="custom-gradient-text">website</span> like
